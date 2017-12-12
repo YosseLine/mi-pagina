7f93f9b1807ee56a4a775813707033c40e54808c
YosseLine <yoseline_96@hotmail.com>
2017-12-12 06:59:45 -0500

    Añadiendo funcionalidad

---
+++ js/app.js
@@ -0,0 +1,9 @@
+window.addEventListener('load', function(e){
+  var button = document.getElementById('button');
+  var menu = document.getElementById('menu');
+
+button.addEventListener('click', function(e){
+  menu.classList.toggle('block');
+  button.classList.add('cambio');
+});
+});
